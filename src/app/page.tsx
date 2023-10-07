4b099b9ea71004e77bdb0f54d4fcd0c7521aa98a
ayub <ayub.alibekov@progforce.com>
2023-10-07 03:37:31 +0300

    init v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,86 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { YMaps, useYMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import { useRef, useState } from "react";
 
-export default function Home() {
+import { AddressSuggestions, DaDataAddress, DaDataSuggestion } from "react-dadata";
+import "react-dadata/dist/react-dadata.css";
+import { adaptToDaDataSuggestion } from "./adapters";
+
+type MapOnLoadParams = ReturnType<typeof useYMaps>;
+
+const INIT_COORDS = [55.75, 37.57];
+const DADATA_TOKEN = "0dfe7fb2ada6cbdde4d36fc90148deb17a4f0cbb";
+const API_KEY = "05f8d2ae-bd94-4329-b9f9-7351e2ec9627";
+
+export default function Page() {
+  const refMap = useRef();
+  const ymapsRef = useRef<MapOnLoadParams>(null);
+
+  const [placemarkCoords, setPlacemarkCoords] = useState(INIT_COORDS);
+
+  const [value, setValue] = useState<DaDataSuggestion<DaDataAddress>>();
+
+  const handleClick = async (e: any) => {
+    const coords = e.get("coords");
+    const res = await ymapsRef.current?.geocode(coords);
+    const firstGeoObject = res?.geoObjects.get(0) as any;
+    if (firstGeoObject) {
+      const address = firstGeoObject.getAddressLine();
+      const city = firstGeoObject.getLocalities().length
+        ? firstGeoObject.getLocalities()
+        : firstGeoObject.getAdministrativeAreas();
+
+      const street = firstGeoObject.getThoroughfare();
+      const house = firstGeoObject.getPremiseNumber();
+
+      setValue(
+        adaptToDaDataSuggestion(address, {
+          city,
+          street,
+          house,
+          geo_lat: coords[0],
+          geo_lon: coords[1],
+        })
+      );
+    }
+    setPlacemarkCoords(coords);
+  };
+
+  const handleMapLoad = (ymaps: MapOnLoadParams) => {
+    ymapsRef.current = ymaps;
+  };
+
+  const handleChange = async (e: DaDataSuggestion<DaDataAddress> | undefined) => {
+    setValue(e);
+    const addrr = e?.unrestricted_value;
+    if (addrr) {
+      const res = await ymapsRef.current?.geocode(addrr);
+      const firstGeoObject = res?.geoObjects.get(0);
+      const c = firstGeoObject?.geometry?.getBounds();
+      if (c && c[0]) {
+        setPlacemarkCoords(c[0]);
+      }
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <YMaps
+      query={{
+        load: "package.full",
+        apikey: API_KEY,
+      }}
+    >
+      <AddressSuggestions token={DADATA_TOKEN} value={value} onChange={handleChange} delay={500} />
+      <Map
+        width="100%"
+        height="80vh"
+        state={{ center: placemarkCoords, zoom: 9 }}
+        instanceRef={refMap}
+        onLoad={handleMapLoad}
+        onClick={handleClick}
+      >
+        <Placemark geometry={placemarkCoords} />
+      </Map>
+    </YMaps>
+  );
 }
